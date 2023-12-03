5cb1d37b7460d0cc2b19c8c01c56833207f0ce2e
sorfeb <soros21febriano@gmail.com>
2023-12-03 09:15:30 +0700

    tweaked registration page

--- lib/main/screens/login.dart
+++ lib/main/screens/login.dart
@@ -21,97 +21,122 @@ class _LoginPageState extends State<LoginPage> {
     Widget build(BuildContext context) {
         final request = context.watch<CookieRequest>();
         return Scaffold(
-            backgroundColor: const Color(0xFF14171C),
-            extendBodyBehindAppBar: true,            // Transparent App Bar
-            appBar: AppBar(
-                backgroundColor: Colors.transparent, // Transparent
-                elevation: 0,                          // No shadow
-                foregroundColor: Colors.white,
-            ),
-            drawer: const LeftDrawer(),
-            body:
-              Stack(
-                children: [
-                  // Background image
-                  Image.asset(
-                    'assets/20000LeaguesUnderTheSeaBookCover.jpg', // Replace with your background image path
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                    height: double.infinity,
-                  ),
-              Container(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                        TextField(
-                            controller: _usernameController,
-                            style: const TextStyle(color: Colors.white),
-                            decoration: const InputDecoration(
-                                labelText: 'Username',
-                            ),
-                        ),
-                        const SizedBox(height: 12.0),
-                        TextField(
-                            controller: _passwordController,
-                            style: const TextStyle(color: Colors.white),
-                            decoration: const InputDecoration(
-                                labelText: 'Password',
-                            ),
-                            obscureText: true,
-                        ),
-                        const SizedBox(height: 24.0),
-                        ElevatedButton(
-                            onPressed: () async {
-                                String username = _usernameController.text;
-                                String password = _passwordController.text;
-
-                                // Cek kredensial
-                                // Untuk menyambungkan Android emulator dengan Django pada localhost,
-                                // gunakan URL http://10.0.2.2/
-                                final response = await request.login("https://readnrate.adaptable.app//auth/login/", {
-                                'username': username,
-                                'password': password,
-                                });
-                    
-                                if (request.loggedIn) {
-                                    String message = response['message'];
-                                    String uname = response['username'];
-                                    usernameGlobal = response['username'];
-                                    Navigator.pushReplacement(
-                                        context,
-                                        MaterialPageRoute(builder: (context) => HomePage()),
-                                    );
-                                    ScaffoldMessenger.of(context)
-                                        ..hideCurrentSnackBar()
-                                        ..showSnackBar(
-                                            SnackBar(content: Text("$message Welcome, $uname.")));
-                                    } else {
-                                    showDialog(
-                                        context: context,
-                                        builder: (context) => AlertDialog(
-                                            title: const Text('Login Failed'),
-                                            content:
-                                                Text(response['message']),
-                                            actions: [
-                                                TextButton(
-                                                    child: const Text('OK'),
-                                                    onPressed: () {
-                                                        Navigator.pop(context);
-                                                    },
-                                                ),
-                                            ],
-                                        ),
-                                    );
-                                }
-                            },
-                            child: const Text('Login'),
-                          ),
-                      ],
-                  ),
+          backgroundColor: const Color(0xFF14171C),
+          extendBodyBehindAppBar: true,            // Transparent App Bar
+          appBar: AppBar(
+              backgroundColor: Colors.transparent, // Transparent
+              elevation: 0,                          // No shadow
+              foregroundColor: Colors.white,
+          ),
+          drawer: const LeftDrawer(),
+          body:
+            Stack(
+              children: [
+                // Background image
+                Image.asset(
+                  'assets/20000LeaguesUnderTheSeaBookCover.jpg', // Replace with your background image path
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: double.infinity,
                 ),
-          ],
-        ),
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Colors.black.withOpacity(0.6),
+                        const Color(0xFF14171C),
+                      ],
+                    ),
+                  ),
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                          //Title
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              "Sign in to ReadNRate",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.w200,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 10.0),
+                          TextField(
+                              controller: _usernameController,
+                              style: const TextStyle(color: Colors.white),
+                              decoration: const InputDecoration(
+                                  labelText: 'Username',
+                                  labelStyle: TextStyle(color:Color(0xFF556678)),
+                              ),
+                          ),
+                          const SizedBox(height: 12.0),
+                          TextField(
+                              controller: _passwordController,
+                              style: const TextStyle(color: Colors.white),
+                              decoration: const InputDecoration(
+                                  labelText: 'Password',
+                                  labelStyle: TextStyle(color:Color(0xFF556678)),
+                              ),
+                              obscureText: true,
+                          ),
+                          const SizedBox(height: 24.0),
+                          ElevatedButton(
+                              onPressed: () async {
+                                  String username = _usernameController.text;
+                                  String password = _passwordController.text;
+
+                                  // Cek kredensial
+                                  // Untuk menyambungkan Android emulator dengan Django pada localhost,
+                                  // gunakan URL http://10.0.2.2/
+                                  final response = await request.login("https://readnrate.adaptable.app//auth/login/", {
+                                  'username': username,
+                                  'password': password,
+                                  });
+                      
+                                  if (request.loggedIn) {
+                                      String message = response['message'];
+                                      String uname = response['username'];
+                                      usernameGlobal = response['username'];
+                                      Navigator.pushReplacement(
+                                          context,
+                                          MaterialPageRoute(builder: (context) => HomePage()),
+                                      );
+                                      ScaffoldMessenger.of(context)
+                                          ..hideCurrentSnackBar()
+                                          ..showSnackBar(
+                                              SnackBar(content: Text("$message Welcome, $uname.")));
+                                      } else {
+                                      showDialog(
+                                          context: context,
+                                          builder: (context) => AlertDialog(
+                                              title: const Text('Login Failed'),
+                                              content:
+                                                  Text(response['message']),
+                                              actions: [
+                                                  TextButton(
+                                                      child: const Text('OK'),
+                                                      onPressed: () {
+                                                          Navigator.pop(context);
+                                                      },
+                                                  ),
+                                              ],
+                                          ),
+                                      );
+                                  }
+                              },
+                              child: const Text('Login'),
+                            ),
+                        ],
+                    ),
+                  ),
+        ],
+      ),
     );
   }
 }
--- lib/main/screens/register.dart
+++ lib/main/screens/register.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:project/home/widget/left_drawer.dart';
 import 'package:provider/provider.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -23,115 +24,135 @@ class _RegisterPageState extends State<RegisterPage> {
       backgroundColor: const Color(0xFF14171C),
       extendBodyBehindAppBar: true,            // Transparent App Bar
       appBar: AppBar(
-        title: const Text('Register'),
         backgroundColor: Colors.transparent, // Transparent
         elevation: 0,                          // No shadow
         foregroundColor: Colors.white,
       ),
-      body: Container(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      drawer: const LeftDrawer(),
+      body:
+        Stack(
           children: [
-            TextField(
-              controller: _usernameController,
-              style: const TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                labelStyle:
-                    const TextStyle(color: Color.fromRGBO(189, 189, 189, 1)),
-                hintText: "Username",
-                labelText: "Username",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0),
+            // Background image
+            Image.asset(
+              'assets/americanPsychoBookCover.webp', // Replace with your background image path
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.black.withOpacity(0.6),
+                    const Color(0xFF14171C),
+                  ],
                 ),
               ),
-            ),
-            const SizedBox(height: 12.0),
-            TextField(
-              controller: _passwordController,
-              style: const TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                labelStyle:
-                    const TextStyle(color: Color.fromRGBO(189, 189, 189, 1)),
-                hintText: "Password",
-                labelText: "Password",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 12.0),
-            TextField(
-              controller: _passwordConfirmationController,
-              style: const TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                labelStyle:
-                    const TextStyle(color: Color.fromRGBO(189, 189, 189, 1)),
-                hintText: "Password Confirmation",
-                labelText: "Password Confirmation",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 24.0),
-            ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all<Color>(Colors.grey.shade700),
-              ),
-              onPressed: () async {
-                String username = _usernameController.text;
-                String password = _passwordController.text;
-                String passwordConfirmation =
-                    _passwordConfirmationController.text;
-
-                final response = await request.postJson(
-                  "https://readnrate.adaptable.app/auth/register",
-                  // "http://localhost:8000/auth/register/",
-                  jsonEncode(<String, String>{
-                    'username': username,
-                    'password': password,
-                    'passwordConfirmation': passwordConfirmation,
-                  }),
-                );
-
-                if (response["status"] == "success") {
-                  Navigator.pop(context); // back to login page
-                  ScaffoldMessenger.of(context)
-                    ..hideCurrentSnackBar()
-                    ..showSnackBar(SnackBar(
-                        content: Text(
-                            "Register berhasil! Akun $username sudah dapat digunakan.")));
-                } else {
-                  showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                      title: const Text('Register Gagal'),
-                      content: Text(response['message']),
-                      actions: [
-                        TextButton(
-                          child: const Text('OK'),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                        ),
-                      ],
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  //Title
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Join ReadNRate",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.w200,
+                      ),
                     ),
-                  );
-                }
-                if (request.loggedIn) {
-                } else {}
-              },
-              child: const Text(
-                'Register',
-                style: TextStyle(color: Colors.white),
+                  ),
+                  const SizedBox(height: 10.0),
+                  TextField(
+                    controller: _usernameController,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: const InputDecoration(
+                      labelStyle: TextStyle(color:Color(0xFF556678)),
+                      labelText: "Username",
+                    ),
+                  ),
+                  const SizedBox(height: 12.0),
+                  TextField(
+                    controller: _passwordController,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(color:Color(0xFF556678)),
+                    ),
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 12.0),
+                  TextField(
+                    controller: _passwordConfirmationController,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: const InputDecoration(
+                      labelText: 'Confirm your Password',
+                      labelStyle: TextStyle(color:Color(0xFF556678)),
+                    ),
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 24.0),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.grey.shade700),
+                    ),
+                    onPressed: () async {
+                      String username = _usernameController.text;
+                      String password = _passwordController.text;
+                      String passwordConfirmation =
+                          _passwordConfirmationController.text;
+
+                      final response = await request.postJson(
+                        "https://readnrate.adaptable.app/auth/register",
+                        // "http://localhost:8000/auth/register/",
+                        jsonEncode(<String, String>{
+                          'username': username,
+                          'password': password,
+                          'passwordConfirmation': passwordConfirmation,
+                        }),
+                      );
+
+                      if (response["status"] == "success") {
+                        Navigator.pop(context); // back to login page
+                        ScaffoldMessenger.of(context)
+                          ..hideCurrentSnackBar()
+                          ..showSnackBar(SnackBar(
+                              content: Text(
+                                  "Register berhasil! Akun $username sudah dapat digunakan.")));
+                      } else {
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: const Text('Register Gagal'),
+                            content: Text(response['message']),
+                            actions: [
+                              TextButton(
+                                child: const Text('OK'),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ],
+                          ),
+                        );
+                      }
+                      if (request.loggedIn) {
+                      } else {}
+                    },
+                    child: const Text(
+                      'Register',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+        ],
       ),
     );
   }
