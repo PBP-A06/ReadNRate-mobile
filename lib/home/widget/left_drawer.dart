0081f1b06f38159994d085713a7b9b4d31d7fb8d
raviehasan <ravie.hasan@ui.ac.id>
2023-12-07 19:21:39 +0700

    screens for "all books"

--- lib/home/widget/left_drawer.dart
+++ lib/home/widget/left_drawer.dart
@@ -1,9 +1,8 @@
-
 import 'package:flutter/material.dart';
 import 'package:project/home/screens/menu.dart';
 import 'package:project/leaderboard/screens/leaderboard_page.dart';
-import 'package:project/user_profile/screens/profile.dart';
 import 'package:project/main/screens/book_list.dart';
+import 'package:project/user_profile/screens/profile.dart';
 import 'package:project/main/screens/login.dart';
 import 'package:project/main/screens/register.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
@@ -21,12 +20,12 @@ class UserProfileWidget extends StatelessWidget {
       children: [
         const Padding(
           padding: EdgeInsets.all(10),
-            child: CircleAvatar(
-              radius: 38,
-              backgroundImage: NetworkImage(
-                  'https://i.pinimg.com/550x/6e/c3/13/6ec313d108678bd6e33d5e6935c3099f.jpg'),
-            ),
+          child: CircleAvatar(
+            radius: 38,
+            backgroundImage: NetworkImage(
+                'https://i.pinimg.com/550x/6e/c3/13/6ec313d108678bd6e33d5e6935c3099f.jpg'),
           ),
+        ),
         Text(
           username,
           style: const TextStyle(
@@ -56,11 +55,12 @@ class LeftDrawer extends StatelessWidget {
                 color: Color(0xFF2C343F),
               ),
               padding: EdgeInsets.all(0),
-              child: UserProfileWidget(username: usernameGlobal!), // Use ! to assert non-null
+              child: UserProfileWidget(
+                  username: usernameGlobal!), // Use ! to assert non-null
             )
           else //USER NOT LOGGED IN
             DrawerHeader(
-              decoration: BoxDecoration(
+              decoration: const BoxDecoration(
                 color: Color(0xFF2C343F),
               ),
               padding: EdgeInsets.all(0),
@@ -92,7 +92,23 @@ class LeftDrawer extends StatelessWidget {
           ),
           ListTile(
             leading: const Icon(
-              Icons.library_books,
+              Icons.checklist,
+              color: Colors.white,
+            ),
+            title: const Text(
+              'All Books',
+              style: TextStyle(color: Colors.white),
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const BooksPage()),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.leaderboard,
               color: Colors.white,
             ),
             title: const Text(
@@ -105,22 +121,6 @@ class LeftDrawer extends StatelessWidget {
                 MaterialPageRoute(
                   builder: (context) => const LeaderboardPage(),
                 ),
-              );
-            },
-          ),
-          ListTile(
-            leading: const Icon(
-              Icons.checklist,
-              color: Colors.white,
-            ),
-            title: const Text(
-              'All Books',
-              style: TextStyle(color: Colors.white),
-            ),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const BooksPage()),
               );
             },
           ),
@@ -153,8 +153,7 @@ class LeftDrawer extends StatelessWidget {
               // Route ke Profile
               Navigator.push(
                 context,
-                MaterialPageRoute(
-                    builder: (context) => const ProfilePage()),
+                MaterialPageRoute(builder: (context) => const ProfilePage()),
               );
             },
           ),
@@ -175,88 +174,88 @@ class LeftDrawer extends StatelessWidget {
                 );
               },
             ),
-            if (usernameGlobal != null)
-              ListTile(
-                leading: const Icon(
-                  Icons.favorite,
-                  color: Colors.white,
-                ),
-                title: const Text(
-                  'Likes',
-                  style: TextStyle(color: Colors.white),
-                ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const BooksPage()),
-                  );
-                },
+          if (usernameGlobal != null)
+            ListTile(
+              leading: const Icon(
+                Icons.favorite,
+                color: Colors.white,
               ),
-            if (usernameGlobal != null)
-              ListTile(
-                leading: const Icon(
-                  Icons.logout,
-                  color: Colors.white,
-                ),
-                title: const Text(
-                  'Logout',
-                  style: TextStyle(color: Colors.white),
-                ),
-                onTap: () async {
-                    final response = await request.logout(
-                        "https://readnrate.adaptable.app/auth/logout/");
-                    String message = response["message"];
-                    usernameGlobal = null;
-                    if (response['status']) {
-                      String uname = response["username"];
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text("$message Sampai jumpa, $uname."),
-                      ));
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => const LoginPage()),
-                      );
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text("$message"),
-                      ));
-                    }
-                },
+              title: const Text(
+                'Likes',
+                style: TextStyle(color: Colors.white),
               ),
-            if (usernameGlobal == null)
-              ListTile(
-                leading: const Icon(
-                  Icons.login,
-                  color: Colors.white,
-                ),
-                title: const Text(
-                  'Sign in',
-                  style: TextStyle(color: Colors.white),
-                ),
-                onTap: () {
-                  Navigator.push(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const BooksPage()),
+                );
+              },
+            ),
+          if (usernameGlobal != null)
+            ListTile(
+              leading: const Icon(
+                Icons.logout,
+                color: Colors.white,
+              ),
+              title: const Text(
+                'Logout',
+                style: TextStyle(color: Colors.white),
+              ),
+              onTap: () async {
+                final response = await request
+                    .logout("https://readnrate.adaptable.app/auth/logout/");
+                String message = response["message"];
+                usernameGlobal = null;
+                if (response['status']) {
+                  String uname = response["username"];
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text("$message Sampai jumpa, $uname."),
+                  ));
+                  Navigator.pushReplacement(
                     context,
                     MaterialPageRoute(builder: (context) => const LoginPage()),
                   );
-                },
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text("$message"),
+                  ));
+                }
+              },
+            ),
+          if (usernameGlobal == null)
+            ListTile(
+              leading: const Icon(
+                Icons.login,
+                color: Colors.white,
               ),
-            if (usernameGlobal == null)
-              ListTile(
-                leading: const Icon(
-                  Icons.person_add,
-                  color: Colors.white,
-                ),
-                title: const Text(
-                  'Register',
-                  style: TextStyle(color: Colors.white),
-                ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const RegisterPage()),
-                  );
-                },
+              title: const Text(
+                'Sign in',
+                style: TextStyle(color: Colors.white),
               ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginPage()),
+                );
+              },
+            ),
+          if (usernameGlobal == null)
+            ListTile(
+              leading: const Icon(
+                Icons.person_add,
+                color: Colors.white,
+              ),
+              title: const Text(
+                'Register',
+                style: TextStyle(color: Colors.white),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const RegisterPage()),
+                );
+              },
+            ),
         ],
       ),
     );
--- lib/main/screens/book_list.dart
+++ lib/main/screens/book_list.dart
@@ -1,92 +1,191 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:project/book/models/book.dart';
 import 'package:project/home/widget/left_drawer.dart';
+import 'package:project/leaderboard/widget/book_leaderboard_card.dart';
 
 class BooksPage extends StatefulWidget {
-    const BooksPage({Key? key}) : super(key: key);
+  const BooksPage({Key? key}) : super(key: key);
 
-    @override
-    _BooksPageState createState() => _BooksPageState();
+  @override
+  _BooksPageState createState() => _BooksPageState();
 }
 
 class _BooksPageState extends State<BooksPage> {
-Future<List<Book>> fetchProduct() async {
-    // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
+  late Future<List<dynamic>> Function() fetchDataFunction; // Book/Readlist
+  String titleText = "Books by Title";
+
+  @override
+  void initState() {
+    super.initState();
+    fetchDataFunction = fetchBooksSortedByTitle; // Initial fetch function
+  }
+
+  Future<List<Book>> fetchBooksSortedByTitle() async {
+    titleText = "Books by Title";
     var url = Uri.parse(
-        'https://readnrate.adaptable.app/books/api/'); 
-        // TODO: URL belum tentu BENAR !!!
+        'https://readnrate.adaptable.app/leaderboard/get-100-books-sorted-by-title/');
     var response = await http.get(
-        url,
-        headers: {"Content-Type": "application/json"},
+      url,
+      headers: {"Content-Type": "application/json"},
     );
 
     // melakukan decode response menjadi bentuk json
     var data = jsonDecode(utf8.decode(response.bodyBytes));
 
-    // melakukan konversi data json menjadi object Product
-    List<Book> list_books = [];
+    // melakukan konversi data json menjadi object Book
+    List<Book> listBook = [];
     for (var d in data) {
-        if (d != null) {
-            list_books.add(Book.fromJson(d));
-        }
+      if (d != null) {
+        listBook.add(Book.fromJson(d));
+      }
     }
-    return list_books;
-}
+    return listBook;
+  }
 
-@override
-Widget build(BuildContext context) {
+  Future<List<Book>> fetchBooksSortedByCategory() async {
+    titleText = "Books by Category";
+    var url = Uri.parse(
+        'https://readnrate.adaptable.app/leaderboard/get-100-books-sorted-by-category/');
+    var response = await http.get(
+      url,
+      headers: {"Content-Type": "application/json"},
+    );
+
+    // melakukan decode response menjadi bentuk json
+    var data = jsonDecode(utf8.decode(response.bodyBytes));
+
+    // melakukan konversi data json menjadi object Book
+    List<Book> listBook = [];
+    for (var d in data) {
+      if (d != null) {
+        listBook.add(Book.fromJson(d));
+      }
+    }
+    return listBook;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.black,
         appBar: AppBar(
-        title: const Text('Product'),
+          title: const Text('Books'),
+          backgroundColor: Colors.grey[800],
+          foregroundColor: Colors.white,
         ),
+        // Masukkan drawer sebagai parameter nilai drawer dari widget Scaffold
         drawer: const LeftDrawer(),
         body: FutureBuilder(
-            future: fetchProduct(),
+            future: fetchDataFunction(),
             builder: (context, AsyncSnapshot snapshot) {
-                if (snapshot.data == null) {
-                    return const Center(child: CircularProgressIndicator());
+              if (snapshot.data == null) {
+                return const Center(child: CircularProgressIndicator());
+              } else {
+                if (!snapshot.hasData) {
+                  return const Column(
+                    children: [
+                      Text(
+                        "Belum ada data.",
+                        style:
+                            TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+                      ),
+                      SizedBox(height: 8),
+                    ],
+                  );
                 } else {
-                    if (!snapshot.hasData) {
-                    return const Column(
+                  return SingleChildScrollView(
+                    // Widget wrapper yang dapat discroll
+                    child: Padding(
+                      padding:
+                          const EdgeInsets.all(5), // Set padding dari halaman
+                      child: Column(
+                        // Widget untuk menampilkan children secara vertikal
                         children: [
-                        Text(
-                            "Tidak ada data produk.",
-                            style:
-                                TextStyle(color: Color(0xff59A5D8), fontSize: 20),
-                        ),
-                        SizedBox(height: 8),
-                        ],
-                    );
-                } else {
-                    return ListView.builder(
-                        itemCount: snapshot.data!.length,
-                        itemBuilder: (_, index) => Container(
-                                margin: const EdgeInsets.symmetric(
-                                    horizontal: 16, vertical: 12),
-                                padding: const EdgeInsets.all(20.0),
-                                child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                    Text(
-                                    "${snapshot.data![index].fields.name}",
-                                    style: const TextStyle(
-                                        fontSize: 18.0,
-                                        fontWeight: FontWeight.bold,
-                                    ),
-                                    ),
-                                    const SizedBox(height: 10),
-                                    Text("${snapshot.data![index].fields.price}"),
-                                    const SizedBox(height: 10),
-                                    Text(
-                                        "${snapshot.data![index].fields.description}")
-                                ],
+                          const SizedBox(height: 10),
+                          // nanti sesuaikan berdasarkan button yang diklik
+                          Text(
+                            titleText,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          const Text(
+                            'Change to sort by?',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                              fontStyle: FontStyle.italic,
+                            ),
+                          ),
+                          const SizedBox(height: 7),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              ElevatedButton(
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.grey.shade700),
                                 ),
-                            ));
-                    }
+                                onPressed: () {
+                                  setState(() {
+                                    fetchDataFunction = fetchBooksSortedByTitle;
+                                  });
+                                },
+                                child: const Text(
+                                  "Title",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 13),
+                                ),
+                              ),
+                              const SizedBox(width: 10),
+                              ElevatedButton(
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.grey.shade700),
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    fetchDataFunction =
+                                        fetchBooksSortedByCategory;
+                                  });
+                                },
+                                child: const Text(
+                                  "Category",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 13),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          // Grid layout
+                          GridView.count(
+                              // Container pada card kita.
+                              primary: true,
+                              padding: const EdgeInsets.all(10),
+                              crossAxisSpacing: 10,
+                              mainAxisSpacing: 10,
+                              crossAxisCount: 2,
+                              shrinkWrap: true,
+                              childAspectRatio: 0.48,
+                              children: (snapshot.data! as List<Book>)
+                                  .map((Book book) {
+                                return BookCard(book);
+                              }).toList()),
+                        ],
+                      ),
+                    ),
+                  );
                 }
+              }
             }));
-    }
+  }
 }
