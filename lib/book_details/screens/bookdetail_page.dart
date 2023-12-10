a211529c77003b7f30715a19baaa823541137745
ffatihzain <fatihzain10@gmail.com>
2023-12-10 20:04:51 +0700

    Tweaked Book Detail UI #3

--- lib/book_details/screens/bookdetail_page.dart
+++ lib/book_details/screens/bookdetail_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project/book/models/book.dart'; // Pastikan mengimpor model Book dengan benar
+import 'package:project/book/models/book.dart';
 
 class BookDetailPage extends StatefulWidget {
   final Book book;
@@ -11,151 +11,216 @@ class BookDetailPage extends StatefulWidget {
 }
 
 class _BookDetailPageState extends State<BookDetailPage> {
-  bool _isReviewVisible = false; // Flag untuk mengatur visibilitas form review
+  bool _isReviewVisible = false;
+  bool _isLiked = false;
+  bool _isBookmarked = false;
+  final TextEditingController _reviewController = TextEditingController();
+  final List<Map<String, String>> _reviews = [];
 
   void _toggleReviewForm() {
     setState(() {
-      _isReviewVisible = !_isReviewVisible; // Mengganti status visibilitas
+      _isReviewVisible = !_isReviewVisible;
     });
+  }
+
+  void _toggleLike() {
+    setState(() {
+      _isLiked = !_isLiked;
+    });
+  }
+
+  void _toggleBookmark() {
+    setState(() {
+      _isBookmarked = !_isBookmarked;
+    });
+  }
+
+  void _submitReview() {
+    if (_reviewController.text.isNotEmpty) {
+      setState(() {
+        _reviews.add({
+          'username': 'xx', // Temporary username
+          'review': _reviewController.text,
+        });
+        _reviewController.clear();
+        _isReviewVisible = false;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _reviewController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    double cmToPixels(double cm) =>
+        cm * 38.8; // Adjust the multiplier according to your screen's dpi
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Book Detail'),
         backgroundColor: Colors.grey[850],
+        foregroundColor: Colors.white,
       ),
-      body: Container(
-        color: Colors.black,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.black,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: cmToPixels(1)),
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Image.network(
-                  widget.book.fields.imageLink, // URL ke gambar sampul buku
-                  width: 150, // Lebar gambar
-                  height: 220, // Tinggi gambar
-                  fit: BoxFit
-                      .cover, // Sesuaikan ukuran gambar dengan batas widget
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          widget.book.fields.title, // Judul buku
-                          style: const TextStyle(
-                            color: Colors.white, // Warna teks
-                            fontSize: 24, // Ukuran font
-                            fontWeight: FontWeight.bold, // Berat font
-                          ),
-                        ),
-                        Row(
+                SizedBox(height: cmToPixels(1)),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.network(
+                      widget.book.fields.imageLink,
+                      width: cmToPixels(7),
+                      height: cmToPixels(10),
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(width: cmToPixels(1)),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.only(top: cmToPixels(1)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            IconButton(
-                              icon: const Icon(
-                                  Icons.favorite), // Ikon berbentuk hati
-                              color: Colors.white,
-                              onPressed: () {}, // Fungsi untuk 'like'
+                            Text(
+                              widget.book.fields.title,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                            IconButton(
-                              icon: const Icon(Icons.bookmark),
-                              color: Colors.white,
-                              onPressed: () {}, // Fungsi untuk 'bookmark'
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                  icon: Icon(_isLiked
+                                      ? Icons.favorite
+                                      : Icons.favorite_border),
+                                  color: _isLiked ? Colors.red : Colors.white,
+                                  onPressed: _toggleLike,
+                                ),
+                                IconButton(
+                                  icon: Icon(_isBookmarked
+                                      ? Icons.bookmark
+                                      : Icons.bookmark_border),
+                                  color: _isBookmarked
+                                      ? Colors.yellow
+                                      : Colors.white,
+                                  onPressed: _toggleBookmark,
+                                ),
+                              ],
                             ),
                           ],
                         ),
-                      ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: cmToPixels(1)),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white24,
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(color: Colors.white),
+                  ),
+                  child: Text(
+                    widget.book.fields.bookDescription,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
                     ),
                   ),
                 ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border(bottom: BorderSide(color: Colors.white)),
+                    ),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: TextButton(
+                        onPressed: _toggleReviewForm,
+                        child: Text(
+                          'Review',
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                if (_isReviewVisible)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0, vertical: 8.0),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      color: Colors.grey[850],
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 16.0, vertical: 8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            TextField(
+                              controller: _reviewController,
+                              maxLines: null,
+                              style: TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                hintText: 'Write your review here...',
+                                hintStyle: TextStyle(
+                                    color: Colors.white.withOpacity(0.6)),
+                                border: InputBorder.none,
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: _submitReview,
+                              child: Text(
+                                'Submit Review',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ..._reviews
+                    .map((review) => ListTile(
+                          title: Text(
+                            review['username']!,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          subtitle: Text(
+                            review['review']!,
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ))
+                    .toList(),
+                SizedBox(
+                    height: 50), // Add more space at the bottom of the page
               ],
             ),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 16.0),
-              padding: const EdgeInsets.all(16.0),
-              decoration: BoxDecoration(
-                color: Colors.white24,
-                borderRadius: BorderRadius.circular(8),
-                border: Border.all(color: Colors.white), // Menambahkan border
-              ),
-              child: Text(
-                widget.book.fields.bookDescription,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-
-            // Padding untuk tombol Review dengan container border
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 0),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border(bottom: BorderSide(color: Colors.white)),
-                ),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: TextButton(
-                    onPressed: _toggleReviewForm,
-                    child: Text(
-                      'Review',
-                      style: TextStyle(
-                        decoration: TextDecoration.underline,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-            // Kondisi untuk menampilkan TextFormField dan TextButton Submit
-            if (_isReviewVisible)
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: TextFormField(
-                      maxLines: 5,
-                      style: TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        hintText: 'Write your review here...',
-                        fillColor: Colors.grey,
-                        filled: true,
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
-                        ),
-                        hintStyle:
-                            TextStyle(color: Colors.white.withOpacity(0.6)),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
-                    child: TextButton(
-                      onPressed: () {
-                        // TODO: Implement submit review action
-                      },
-                      child: Text(
-                        'Submit Review',
-                        style: TextStyle(
-                          color: Colors.white,
-                          decoration: TextDecoration.underline,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-          ],
+          ),
         ),
       ),
     );
